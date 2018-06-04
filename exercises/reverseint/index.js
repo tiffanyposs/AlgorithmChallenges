1ea84ef9ec94e52f0f3589ec28a22d18c02a7366
Tiffany Poss <tiffanyposs@gmail.com>
2018-06-03 22:55:48 -0400

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,38 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n
+     .toString()
+     .split('')
+     .reverse()
+     .join('');
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// Tiffany second solution
+// function reverseInt(n) {
+//   const arr = n.toString().split('');
+//   if (arr.length === 1) return n;
+//   let multiplier = Math.sign(n);
+//   if (multiplier === -1) arr.shift();
+//   arr.reverse();
+//   return parseInt(arr.join(''))*multiplier;
+// }
+
+// Tiffany original solution
+// function reverseInt(n) {
+//   const arr = n.toString().split('');
+//   if (arr.length === 1) return n;
+//
+//   let multiplier = 1;
+//   if (arr[0] === '-') {
+//     multiplier = -1;
+//     arr.shift()
+//   }
+//   arr.reverse();
+//   const newN = parseInt(arr.join(''));
+//   return newN*multiplier;
+// }
