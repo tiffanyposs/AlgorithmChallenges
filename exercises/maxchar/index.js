4dfcf83da37b7a164490b8ad77d53f6bd13e1b57
Tiffany Poss <tiffanyposs@gmail.com>
2018-06-04 22:18:08 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,41 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for(let key in charMap) {
+    if(charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+//
+// function maxChar(str) {
+//   const obj = {};
+//   str.split('').forEach(char => {
+//     if(obj.hasOwnProperty(char)) {
+//       obj[char]++;
+//     } else {
+//       obj[char] = 1;
+//     }
+//   });
+//   let largest = null;
+//   for(var key in obj) {
+//     if(largest === null || obj[key] > obj[largest]) {
+//       largest = key
+//     }
+//   }
+//   return largest;
+// }
