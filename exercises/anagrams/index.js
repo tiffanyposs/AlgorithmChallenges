cdbd1ff7d9ddf83c039ebcd0ff127d1181c28807
Tiffany Poss <tiffanyposs@.com>
2018-11-26 12:49:28 -0500

    solve chunk and anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,51 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function cleanString(str) {
+	return str
+		.replace(/[^\w]/g, '')
+		.toLowerCase()
+		.split('')
+		.sort()
+		.join('');
+}
+
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+// // MY ORIGINAL SOLUTION
+// function anagrams(stringA, stringB) {
+// 	stringA = stringA.replace(/[^a-z0-9]/g, '').toLowerCase();
+// 	stringB = stringB.replace(/[^a-z0-9]/g, '').toLowerCase();
+//
+// 	stringA = stringA.split('').sort().join('');
+// 	stringB = stringB.split('').sort().join('');
+//
+// 	return stringA === stringB;
+// }
+
+
+// // SOLUTION 2
+// function buildCharMap(str) {
+// 	const charMap = {};
+// 	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	}
+// 	return charMap;
+// }
+//
+// function anagrams(stringA, stringB) {
+// 	const aCharMap = buildCharMap(stringA);
+// 	const bCharMap = buildCharMap(stringB);
+//
+// 	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) return false;
+//
+// 	for (let key in aCharMap) {
+// 		if (aCharMap[key] !== bCharMap[key]) return false;
+// 	}
+//
+// 	return true;
+// }
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+
+// MY SOLUTION
+function chunk(array, size) {
+	const chunked = [];
+	while (array.length) chunked.push(array.splice(0, size));
+	return chunked;
+}
 
 module.exports = chunk;
+
+// BETTER SOLUTION
+// function chunk(array, size) {
+// 	const chunked = [];
+// 	let index = 0;
+// 	while(index <  array.length) {
+// 		chunked.push(array.slice(index, index + size));
+// 		index+=size;
+// 	}
+// 	return chunked;
+// }
+
+
+// INITIAL SOLUTION
+// function chunk(array, size) {
+// 	const chunked = [];
+// 	for (let element of array) {
+// 		const last = chunked[chunked.length - 1];
+// 		if (!last || last.length === size) {
+// 			chunked.push([element]);
+// 		} else {
+// 			last.push(element);
+// 		}
+// 	}
+// 	return chunked;
+// }
