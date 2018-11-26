786035ccf36aa5781384986b5536471cd09221da
Tiffany Poss <tiffanyposs@.com>
2018-11-26 13:26:28 -0500

    capitalize solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,45 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalizeFirstLetter(str) {
+	return str[0].toUpperCase() + str.slice(1);
+}
+
+function capitalize(str) {
+	return str.split(' ').map(capitalizeFirstLetter).join(' ');
+}
 
 module.exports = capitalize;
+
+// // My Original Solution
+// function capitalizeFirstLetter(str) {
+// 	return str[0].toUpperCase() + str.slice(1);
+// }
+//
+// function capitalize(str) {
+// 	return str.split(' ').map(capitalizeFirstLetter).join(' ');
+// }
+
+// // Class solution 1
+// function capitalize(str) {
+// 	const words = [];
+//
+// 	for (let word of str.split(' ')) {
+// 		words.push(word[0].toUpperCase() + word.slice(1));
+// 	}
+//
+// 	return words.join(' ');
+// }
+
+// // Class solution 2
+// function capitalize(str) {
+// 	let result = str[0].toUpperCase();
+// 	for (let i = 1; i < str.length; i++) {
+// 		if (str[i - 1] === ' ') {
+// 			result += str[i].toUpperCase();
+// 		} else {
+// 			result += str[i];
+// 		}
+// 	}
+// 	return result;
+// }
