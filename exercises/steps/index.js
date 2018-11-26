2272a740e4486f4ce6b61c1e9937b3aa91e838ee
Tiffany Poss <tiffanyposs@.com>
2018-11-26 14:24:34 -0500

    solution for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,52 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+function steps(total, step = 1) {
+	if (step > total) return;
+	const stair = '#'.repeat(step) + ' '.repeat(total - step);
+	console.log(stair);
+	steps(total, step + 1);
+}
 
 module.exports = steps;
+
+// // NOTE My Original Solution
+// function steps(n) {
+// 	for (let i = 1; i <= n; i++) {
+// 		let step = '#'.repeat(i) + ' '.repeat(n - i);
+// 		console.log(step);
+// 	}
+// }
+
+// // NOTE My Recursive Solution
+// function steps(total, step = 1) {
+// 	if (step > total) return;
+// 	const stair = '#'.repeat(step) + ' '.repeat(total - step);
+// 	console.log(stair);
+// 	steps(total, step + 1);
+// }
+
+// // NOTE Class Solution 1
+// function steps(n) {
+// 	for (let row = 0; row < n; row++) {
+// 		let stair = '';
+// 		for (let column = 0; column < n; column++) {
+// 			if (column <= row) stair += '#';
+// 			else stair += ' ';
+// 		}
+// 		console.log(stair);
+// 	}
+// }
+
+// // NOTE Class Solution 2 - Recursive
+// function steps(n, row = 0, stair = '') {
+// 	if (n === row) return;
+// 	if (n === stair.length) {
+// 		console.log(stair);
+// 		return steps(n, row + 1);
+// 	}
+// const add = stair.length <= row ? '#' : ' ';
+//
+// 	steps(n, row, stair + add);
+// }
